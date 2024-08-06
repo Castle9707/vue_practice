d32694ba5660f8890b9c8b4c90543a21b44efa94
Castle9707 <paomincc25@gmail.com>
2024-08-06 11:21:37 +0800

    按鈕事件

--- practice/form_validation/src/components/Register.vue
+++ practice/form_validation/src/components/Register.vue
@@ -1,32 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import { ref,reactive } from 'vue'
 const name = ref('')
 const mobile = ref('')
+const birthday = ref('')
+const warning = reactive([true,true,true])
 </script>
 
 <template>
 <form>
   <div class="mb-3">
     <label for="name" class="form-label">姓名</label>
-    <input type="text" class="form-control" id="name" aria-describedby="nameHelp">
-    <div id="nameHelp" class="form-text">請輸入姓名</div>
+    <input type="text" v-model="name" class="form-control" id="name" aria-describedby="nameHelp">
+    <div id="nameHelp" class="form-text text-danger" :class="{'d-none':warning[0]}">請輸入姓名</div>
   </div>
   <div class="mb-3">
     <label for="mobile" class="form-label">電話</label>
-    <input type="text" class="form-control" id="mobile" aria-describedby="mobileHelp">
-    <div id="mobileHelp" class="form-text">請輸入電話</div>
+    <input type="text" v-model="mobile" class="form-control" id="mobile" aria-describedby="mobileHelp">
+    <div id="mobileHelp" class="form-text text-danger" :class="{'d-none':warning[1]}">請輸入電話</div>
+  </div>
+  <div class="mb-3">
+    <label for="birthday" class="form-label">生日</label>
+    <input type="date" v-model="birthday" class="form-control" id="birthday" aria-describedby="birthdayHelp">
+    <div id="birthdayHelp" class="form-text text-danger" :class="{'d-none':warning[2]}">請輸入生日</div>
   </div>
 
   <div class="form-check">
   <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
   <label class="form-check-label" for="flexRadioDefault1">
-    Default radio
+    男
   </label>
 </div>
 <div class="form-check">
   <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
   <label class="form-check-label" for="flexRadioDefault2">
-    Default checked radio
+    女
   </label>
 </div>
   <div class="mb-3 form-check">
@@ -38,4 +45,7 @@ const mobile = ref('')
 </template>
 
 <style scoped>
+.text-danger{
+  color: red;
+}
 </style>
---
+++ practice/my_first_app/src/components/ButtonEvent.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+const boxColor = ref('initial')
+const msg = ref('')
+
+const changeColor = (color) =>{
+  boxColor.value = color
+}
+const showMsg = (e) => {
+  msg.value = '你按到enter啦的哈哈哈'
+  alert(msg.value)
+}
+</script>
+
+<template>
+<div id="box" :style="{ backgroundColor: boxColor }"></div>
+  <div class="container">
+  <button @click.stop="changeColor('red')">Red</button>
+  <button @click.prevent="changeColor('green')">Green</button>
+  <button @click.capture="changeColor('yellow')">Yellow</button>
+  <button @click.self="changeColor('tomato')">Tomato</button>
+  <button @click.middle="changeColor('orange')">按滑鼠滾輪變成Orange</button>
+  <button @click.once="changeColor('purple')">Purple只能變一次</button>
+  <button @wheel.passive="changeColor('pink')">Pink</button>
+  <input @keydown.enter="showMsg(e)" >
+</div>
+</template>
+
+<style scoped>
+#box{
+  margin-bottom: 20px;
+  width: 200px;
+  height: 200px;
+}
+
+button{
+  color: aliceblue;
+  background-color: rgb(92, 168, 234);
+  margin-right: 16px;
+}
+</style>
