4bd90a3ff4639196009a91d0dac52153aaf96bad
Castle9707 <paomincc25@gmail.com>
2024-08-06 13:53:26 +0800

    父子組件傳值

--- practice/form_validation/src/App.vue
+++ practice/form_validation/src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
-import Register from './components/Register.vue'
+// import Register from './components/Register.vue'
+import DataSend from './components/DataSend.vue'
+import { ref } from 'vue'
+const appMsg = ref('這是來自APP的訊息')
+const money = ref(50000)
+let receiveMsg = ref('預設訊息')
+function updateMsg(newMsg){
+  receiveMsg.value = newMsg
+}
 </script>
 
 <template>
-<Register></Register>
+<!-- <Register /> -->
+<p>子組件傳來的訊息</p>
+<p>{{receiveMsg}}</p>
+ <DataSend :msg="appMsg" :money="money" @update:msg="updateMsg" />
 </template>
 
 <style scoped>
---
+++ practice/form_validation/src/components/DataSend.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  msg: String,
+  money: Number
+})
+let showMsg = ref(props.msg)
+let salary = ref(props.money)
+const emit = defineEmits(['update:msg'])
+const sendMsg = () => {
+  emit('update:msg', showMsg)
+}
+</script>
+
+<template>
+  <h2>{{ showMsg }}</h2>
+  <h2>{{ salary }}</h2>
+  <input type="text" v-model="showMsg">
+  <button type="button" @click="sendMsg">送訊息到父元件</button>
+</template>
+
+<style scoped>
+h2{
+  padding: 10px;
+  border: 1px solid blueviolet;
+}
+</style>
